c572368b19373b757b7b4372e3023d8c1aa210a6
Barnicle <egalimov95@gmail.com>
2020-07-03 14:42:41 +0300

    global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {ConnectedRouter} from "connected-react-router";
+import { ConnectedRouter } from "connected-react-router";
 import App from "./components/app";
 import { Provider } from "react-redux";
-import store, {history} from "./store/store";
-import "./styles/index.scss";
-
+import store, { history } from "./store/store";
 
 ReactDOM.render(
   <Provider store={store}>
